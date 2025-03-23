3b1f8897fe044d43facee066f6f9c7cb013deda2
EdoBagus <79005016+Edo-Bagus@users.noreply.github.com>
2025-03-23 12:31:54 +0700

    Update tailwind.config.ts
    fix bug for ingridients and upload recipe page

--- culina-craft/tailwind.config.ts
+++ culina-craft/tailwind.config.ts
@@ -2,16 +2,20 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages//*.{js,ts,jsx,tsx,mdx}",
-    "./src/components//*.{js,ts,jsx,tsx,mdx}",
-    "./src/app//*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        background: "var(--backgrou nd)",
+        foreground: "var(--foreground)",
+      },
       fontFamily: {
-        poppins: ["var(--font-poppins)", "sans-serif"],
-      }
+        poppins: ["var(--font-poppins)", "sans-serif"], // Menambahkan Poppins sebagai font utama
+      },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config;
